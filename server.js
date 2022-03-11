23a6d92278bfba829c993b280b8c4c58599c0a10
Akash Limbu <akashwanem@hotmail.com>
2022-03-11 23:04:52 +0000

    added middleware and port

--- server.js
+++ server.js
@@ -2,3 +2,25 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+
+// sets port to listen
+const PORT = 3001;
+
+// sets up the app
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+
+
+
+
+// this listens to the port after deployment
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+  });
+  
